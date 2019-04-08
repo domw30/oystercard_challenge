fad6b5c55fb0b20278b89e4ca365d052c153c7de
Kintaro-Oe <marcusclairmont@gmail.com>
2019-04-08 17:35:34 +0100

    add default balance for Oystercard.new

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,5 +1,10 @@
 require 'oystercard'
 
 describe Oystercard do
-
+  it "responds to #balance" do
+    expect(subject).to respond_to(:balance)
+  end
+  it "expects a default balance" do
+    expect(subject.balance).to eq 0
+  end
 end
