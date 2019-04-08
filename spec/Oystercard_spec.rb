7d093744e824c4519d41eb843d269914277070f1
Kintaro-Oe <marcusclairmont@gmail.com>
2019-04-08 17:54:21 +0100

    refactor spec file

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,9 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
-  it "responds to #balance" do
-    expect(subject).to respond_to(:balance)
-  end
+  it { is_expected.to respond_to(:balance) }
+
   it "expects a default balance" do
     expect(subject.balance).to eq 0
   end
