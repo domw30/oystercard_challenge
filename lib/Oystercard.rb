b3ab37e38d51e73e7d02eef35d8787ed0c93e91a
domw30 <domw30@gmail.com>
2019-04-09 14:31:32 +0100

    add top_up method, add raise exception for maximum balance

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,7 +1,14 @@
 class Oystercard
   attr_reader :balance
 
+MAXIMUM_BALANCE = 90
+
   def initialize
     @balance = 0
+  end
+
+  def top_up(amount)
+    raise "£#{MAXIMUM_BALANCE}, maximum exceeded" if @balance + amount > MAXIMUM_BALANCE
+    @balance += amount
   end
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -3,7 +3,20 @@ require 'oystercard'
 describe Oystercard do
   it { is_expected.to respond_to(:balance) }
 
+  describe '#balance' do
   it "expects a default balance" do
     expect(subject.balance).to eq 0
+  end
+end
+
+  describe '#top_up' do
+    it "increases the balance value" do
+      expect { subject.top_up(10) }.to change { subject.balance }.by 10
+    end
+    it "raise error if balance exceeds 90" do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect { subject.top_up(1) }.to raise_error "£#{maximum_balance}, maximum exceeded"
+    end
   end
 end
